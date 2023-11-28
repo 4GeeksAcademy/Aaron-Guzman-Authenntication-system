85117f1e10eadcb11a65138d30bfed9f77684bd1
Aarón Guzmán <140634896+aaronguf@users.noreply.github.com>
2023-11-28 20:27:31 +0000

    Working on signup path

--- src/api/routes.py
+++ src/api/routes.py
@@ -20,3 +20,26 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/signup', methods=['POST'])
+def create_user(token):
+    try:
+        email = request.json.get('email')
+        name = request.json.get('name')
+        birth_date = request.json.get('birth_date')
+        password = request.json.get('password')
+
+        if not email or not name or not birth_date or not password:
+            return jsonify({'error': 'All fields are required'}), 400
+        
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            return jsonify({'error': 'User already exist'}), 409
+        
+        
+        
+
+    except Exception as error:
+        return  jsonify({'error': 'Error in user creation: ' + str(error)}), 500
+
