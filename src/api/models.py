0ad9a944238413032f9bccda9aecc5fc7212f8af
Aarón Guzmán <140634896+aaronguf@users.noreply.github.com>
2023-11-30 18:36:24 +0000

    login done not tested I have a bug when migrate

--- src/api/models.py
+++ src/api/models.py
@@ -7,8 +7,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    birth_date = db.Column(db.Date, unique=False, nullable=False) 
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    address = db.Column(db.String(120), unique=False, nullable=False)
+    birth_date = db.Column(db.String(50), unique=False, nullable=False) 
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,6 +17,9 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            "name": self.name
+            "name": self.name,
+            "address": self.address,
+            "birth_date": self.birth_date
+
             # do not serialize the password, its a security breach
         }
